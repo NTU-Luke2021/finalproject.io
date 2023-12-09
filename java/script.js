77379665cdb41101fe583b8932445e0362992c49
Leon <leonhsu59@gmail.com>
2023-12-09 21:26:52 +0800

    Update

---
+++ java/script.js
@@ -0,0 +1,35 @@
+$(function() {
+    const videoElement = $('#video1').get(0);
+  
+    if (!videoElement) {
+      console.error("Video element not found");
+      return;
+    }
+  
+    const videoStart = $('#video1container').offset().top; // 影片division的頂部位置
+    const videoEnd = videoStart + $('#video1container').height(); // 影片division的底部位置
+    const maxScroll = $(document).height() - $(window).height();
+  
+    $(window).scroll(function() {
+      const currentScrollPosition = $(document).scrollTop();
+  
+      if (currentScrollPosition >= videoStart && currentScrollPosition <= videoEnd) {
+        // 影片division在畫面中
+        if (videoElement.paused) {
+          videoElement.play();
+        }
+      } else {
+        // 影片division不在畫面中
+        if (!videoElement.paused) {
+          videoElement.pause();
+        }
+      }
+  
+      if (videoElement.duration) {
+        videoElement.currentTime = videoElement.duration * (currentScrollPosition - videoStart) / (videoEnd - videoStart);
+      } else {
+        console.warn("Video duration is not available");
+      }
+    });
+  });
+  
