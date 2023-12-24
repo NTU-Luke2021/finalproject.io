b847f880d3f10ae0f9ea57cb80222bdfdbec6623
Leon <leonhsu59@gmail.com>
2023-12-24 16:21:40 +0800

    Img

--- java/file.js
+++ java/file.js
@@ -33,7 +33,18 @@ $(function() {
     });
   });
 
+document.addEventListener("DOMContentLoaded", function() {
+  var videoRita = document.getElementById("video2");
   
+  videoRita.addEventListener("ended", function() {
+    document.body.style.overflowY = "hidden";
+  });
+  videoRita.addEventListener("ended", function() {
+        // Allow scrolling once the video ends
+    document.body.style.overflowY = "auto";
+  });
+});
+
  
 /* $(function() {
   const videoElement = $('#video2').get(0);
