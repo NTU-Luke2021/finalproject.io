296633938efbc496821ee3b52bb7607e1a87c405
Leon <leonhsu59@gmail.com>
2023-12-26 16:52:47 +0800

    Update

--- java/file.js
+++ java/file.js
@@ -104,6 +104,41 @@ $(function() {
    });
  });
 
+ $(function() {
+  const videoElement = $('#video4').get(0);
+  
+  if (!videoElement) {
+    console.error("Video element not found");
+    return;
+  }
+  
+  const videoStart = $('#video4container').offset().top; // 影片division的頂部位置
+  const videoEnd = videoStart + $('#video4container').height(); // 影片division的底部位置
+  const maxScroll = $(document).height() - $(window).height();
+  
+  $(window).scroll(function() {
+    const currentScrollPosition = $(document).scrollTop();
+  
+    if (currentScrollPosition >= videoStart && currentScrollPosition <= videoEnd) {
+      // 影片division在畫面中
+      if (videoElement.paused) {
+        videoElement.play();
+      }
+     } else {
+      // 影片division不在畫面中
+      if (!videoElement.paused) {
+         videoElement.pause();
+      }
+    }
+  
+     if (videoElement.duration) {
+       videoElement.currentTime = videoElement.duration * (currentScrollPosition - videoStart) / (videoEnd - videoStart);
+     } else {
+       console.warn("Video duration is not available");
+     }
+   });
+ });
+
 function toggleDropdown() {
   var dropdownContent = document.getElementById("drop1");
   if (dropdownContent.style.display === "block") {
